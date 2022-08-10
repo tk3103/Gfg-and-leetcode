3fc983b50b8caa946ce4809532c3e3d7ee2485ba
tk3103 <77692937+tk3103@users.noreply.github.com>
2022-08-10 11:05:40 +0530

    Create StringPalindrome.java

---
+++ StringPalindrome.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.isPalindrome(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int isPalindrome(String S) {
+        String rev="";
+       for(int i=S.length()-1;i>=0;i--){
+           rev=rev+S.charAt(i);
+       }
+       if(S.equals(rev)){
+           return 1;
+       }
+       return 0;
+    }
+};
